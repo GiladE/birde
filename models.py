344dcd8c46b41eac12ddc696ff3e51f5bff27f8f
= <=>
2015-04-01 15:21:46 +0200

    still rolling

--- models.py
+++ models.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Message(models.Model):
+    owner = models.ForeignKey(User)
     sender = models.CharField(max_length=200)
     recipient = models.CharField(max_length=200)
     dateSent = models.DateTimeField('date sent')
     subject = models.CharField(max_length=500)
     body = models.CharField(max_length=50000)
     type = models.BooleanField()
-    owner = models.ForeignKey(User)
+
+
