5eedb3ddbfb379ae3736970031c198ec2fb4d704
= <=>
2015-04-08 15:12:32 +0200

    Saving sent messages complete and looks perty

--- birde/views.py
+++ birde/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from birde.models import Message,Connection,User
 from django.http import HttpResponse
-import json
+import json, datetime
 from django.views.decorators.csrf import csrf_exempt
 
-
+import smtp_client
 import pop_client
 
 
@@ -18,8 +18,12 @@ def index(request):
 
 @csrf_exempt
 def send(request):
-	msgTo = request.POST.get('email')
-	msgSubject = request.POST.get('subject')
-	msgBody = request.POST.get('message')
-	response_data={"response":"250"}
-	return HttpResponse(json.dumps(response_data),content_type='application/json')
+    msgTo = request.POST.get('email')
+    msgSubject = request.POST.get('subject')
+    msgBody = request.POST.get('message')
+    con_details=Connection.objects.get(user_id=User.objects.get(username="root"))
+    communications = smtp_client.send_mail(con_details.smtpHost,con_details.smtpPort,con_details.smtpUser,con_details.smtpPass,msgTo,msgSubject,msgBody)
+    response_data={"response":communications[-1][0:3]}
+    savedMsg = Message(sender=con_details.smtpUser,recipient=msgTo,dateSent=datetime.datetime.now(),subject=msgSubject,body=msgBody,type=True,owner_id=User.objects.get(username="root").id)
+    savedMsg.save()
+    return HttpResponse(json.dumps(response_data),content_type='application/json')
